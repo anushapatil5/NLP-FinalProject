bc8405ddc273c5415449c5b8c5ea1a5e02ea2db8
abdulrahimq <abdulrahim.qaddoumi@gmail.com>
2020-11-25 22:30:25 -0500

    finalized generation script

--- Notebooks/data_generation.py
+++ Notebooks/data_generation.py
@@ -1,4 +1,3 @@
-
 """
 1- Download the WikiPedia Dump for the language:
 English:
@@ -22,13 +21,18 @@ cd /Users/abdulrahimqaddoumi/opt/anaconda3/lib/python3.7/site-packages/wikiextra
 4- Run this command: data is the target folder and /Users/abdulrahimqaddoumi/... is the dump location.
 python WikiExtractor.py -o data /Users/abdulrahimqaddoumi/Downloads/hiwiki-20201020-pages-articles-multistream.xml.bz2
 
-6- The results should be Folders starting from AA, AB, AC, ... etc. Each folder will have text files named wiki_00, wiki_01, ... etc.
-7- Keep only the folders from AA-AE to keep things consistent because Hindi only have ~500MBs worth of data.
+5- The results should be Folders starting from AA, AB, AC, ... etc. Each folder will have text files named wiki_00, wiki_01, ... etc.
+
+6- Keep only the folders from AA-AE to keep things consistent because Hindi only have ~500MBs worth of data.
 """
+
 
 import os
 import json
 PATH = "/Users/abdulrahimqaddoumi/opt/anaconda3/lib/python3.7/site-packages/wikiextractor/data"
+LANGUAGES = ["en", "hi", "ar", "it"]
+
+
 files_path = []
 for subdir, dir, files in os.walk(PATH):
     for file in files:
@@ -37,7 +41,7 @@ for subdir, dir, files in os.walk(PATH):
 
 
 texts = ""
-for file in files_path[:3]:
+for file in files_path:
     f = open(file, "r")
     texts += f.read()
 
@@ -52,13 +56,19 @@ for text in texts:
 
 training_text = clean_texts[:int(len(clean_texts) * 0.8)]
 remaining_text = clean_texts[int(len(clean_texts) * 0.8):]
-test_text = remaining_text[:len(clean_texts ) // 2]
-valid_text = remaining_text[len(clean_texts ) // 2:]
+test_text = remaining_text[:len(remaining_text) // 2]
+valid_text = remaining_text[len(remaining_text) // 2:]
+all_text = {"train": training_text, "test": test_text, "valid": valid_text}
 
+
+# TODO: Change LANGUAGES to change name
 data = []
-for sentence in training_text.split("."):
-    data.append({"tokens": sentence.split(" ")})
+for key, text in all_text.items():
+    for sentence in text.split("."):
+        data.append({"tokens": sentence.split(" ")})
+    name = LANGUAGES[1] + "_" + key + '.jsonl'
+    with open(name, 'w') as outfile:
+        json.dump(data, outfile)
 
-with open('train.jsonl', 'w') as outfile:
-    json.dump(data, outfile)
+
 print("DONE")
